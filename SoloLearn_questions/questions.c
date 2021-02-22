a985bfb9050734ffaf9878f8f751d0310031770c
McLutzifer <lukasschumi@gmx.at>
2021-02-22 20:18:19 +0100

    update

--- SoloLearn_questions/questions.c
+++ SoloLearn_questions/questions.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int n[5] = {4,6,8};
-    printf("%d", n[4]);
+    //int n[5] = {4,6,8};
+    //printf("%d", n[4]);
+
+    char p[20];
+    char *s = "string";
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        p[i] = s[length - i];
+    }
+    printf("%s",p);
 
     return 0;
 }
