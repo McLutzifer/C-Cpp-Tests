e00a58787b12881aa6386ff60fcfa2a6b3a1be65
McLutzifer <lukasschumi@gmx.at>
2021-02-22 20:23:31 +0100

    add loop function

--- SoloLearn_questions/questions.c
+++ SoloLearn_questions/questions.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+int loopy(int);
+
 int main(void)
 {
     //int n[5] = {4,6,8};
     //printf("%d", n[4]);
 
+    /*
     char p[20];
     char *s = "string";
     int length = strlen(s);
@@ -14,6 +17,22 @@ int main(void)
         p[i] = s[length - i];
     }
     printf("%s",p);
+    */
+
+   loopy(5);
 
     return 0;
+}
+
+int loopy (int i)
+{
+    if (i==0)
+    {
+        printf("%i", i);
+    }   
+    else
+    {
+        loopy(i - 1);
+        printf("%d", i);
+    }
 }
