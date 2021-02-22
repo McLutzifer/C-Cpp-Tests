fe2d7ce5fa73c443afb6e558832d2ab8291587f5
McLutzifer <lukasschumi@gmx.at>
2021-02-22 20:36:58 +0100

    add pointer

--- SoloLearn_questions/main.c
+++ SoloLearn_questions/main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int loopy(int);
+
+int main(void)
+{
+    //int n[5] = {4,6,8};
+    //printf("%d", n[4]);
+
+    /*
+    char p[20];
+    char *s = "string";
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        p[i] = s[length - i];
+    }
+    printf("%s",p);
+    */
+
+    //loopy(5);
+
+    int *p = (int *)malloc(sizeof(int)*4);
+    *p = 42;
+    *(p + 2) = 43;
+    int* v = p;
+    *(v + 1) = 44;
+    printf("%d", *(v+2));
+
     return 0;
+}
+
+int loopy (int i)
+{
+    if (i==0)
+    {
+        printf("%i", i);
+    }
+    else
+    {
+        loopy(i - 1);
+        printf("%d", i);
+    }
 }
