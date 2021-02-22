01df50cdb6a1982e3bfdc2db223a0fe0d5b07fee
McLutzifer <lukasschumi@gmx.at>
2021-02-22 20:31:22 +0100

    create file in CLion - update

---
+++ SoloLearn_questions/cmake-build-debug/CMakeFiles/SoloLearn_questions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoloLearn_questions.dir/main.c.o"
+  "SoloLearn_questions"
+  "SoloLearn_questions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SoloLearn_questions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
